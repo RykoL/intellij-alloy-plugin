1aed0a9a8c265c031c06956b02dd54fc44af389e
Rico Lang <rico.lang@codecentric.de>
2025-01-01 20:19:11 +0100

    Rename test case

--- src/test/kotlin/de/rlang/intellij/alloy/AlloyParserTest.kt
+++ src/test/kotlin/de/rlang/intellij/alloy/AlloyParserTest.kt
@@ -1,15 +1,10 @@
 package de.rlang.intellij.alloy
 
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.ParsingTestCase
 import com.intellij.testFramework.TestDataPath
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.util.PsiErrorElementUtil
-import junit.framework.TestCase
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
-class AlloyPluginTest : ParsingTestCase("", "alloy", AlloyParserDefinition()) {
+class AlloyParserTest : ParsingTestCase("", "alloy", AlloyParserDefinition()) {
 
     fun testParsingData() {
         doTest(true)
