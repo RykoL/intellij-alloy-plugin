4650f5be739f58d70dce777956d6523981f00719
Rico Lang <rico.lang@codecentric.de>
2025-03-11 21:24:39 +0100

    feat: Add tests for spacing around equals sign

--- src/test/kotlin/de/rlang/intellij/alloy/formatter/FormatterTest.kt
+++ src/test/kotlin/de/rlang/intellij/alloy/formatter/FormatterTest.kt
@@ -10,6 +10,14 @@ class FormatterTest : FormatterTestCase() {
         doTest("unformatted", "formatted")
     }
 
+    fun `test adds space before and after equals sign`() {
+        doTextTest("remote_write=http://localhost:9090", "remote_write = http://localhost:9090")
+    }
+
+    fun `test does not add additional space around equals sign`() {
+        doTextTest("remote_write = http://localhost:9090", "remote_write = http://localhost:9090")
+    }
+
     override fun getFileExtension(): String {
         return "alloy"
     }
